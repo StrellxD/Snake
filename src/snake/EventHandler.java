a38c4c40f0ff892dffcdfd504dc7411ba3d41735
Владислав Л. Вандич <Vladislav.Vandich@asud.kis.minsk>
2018-04-04 15:24:03 +0300

    123

--- src/snake/EventHandler.java
+++ src/snake/EventHandler.java
@@ -5,11 +5,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class EventHandler {                                       //Обработчик событий
+public class EventHandler {                                       //Обработчик событий (хз куда его засунуть, я решил отдельный класс создать)
     public static class NewGameButton implements ActionListener { //класс для кнопки "Новая игра"
         public void actionPerformed(ActionEvent e) {              //внутри этого метда код, по нажатию кнопки "Новая игра"
-
-
 
             EventQueue.invokeLater(new Runnable() {
                 @Override
@@ -18,6 +16,12 @@ public class EventHandler {                                       //Обрабо
                     ex.setVisible(true);
                 }
             });
+        }
+    }
+
+    public static class exitButton implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
         }
     }
 }
--- src/snake/Menu.java
+++ src/snake/Menu.java
@@ -5,11 +5,14 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Menu extends JFrame {
+
+    public static boolean visible;
+
     public Menu() {
         super("Snake");
         setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null);                                              //Окно в центре
 
         JPanel jPanel = new JPanel();
         jPanel.setLayout(new GridLayout(3, 1, 5, 5));
@@ -23,7 +26,6 @@ public class Menu extends JFrame {
         jPanel.add(exitButton);
 
         setContentPane(jPanel);
-
         ActionListener actionListener = new EventHandler.NewGameButton();
         newGameButton.addActionListener(actionListener);
     }
